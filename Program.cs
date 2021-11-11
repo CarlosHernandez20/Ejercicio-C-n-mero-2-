6f7c538ffda73fbc55c26d2ea83467e5fb679fae
CarlosHernandez20 <92899918+CarlosHernandez20@users.noreply.github.com>
2021-11-11 18:09:43 -0400

    Ejercicio número dos

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Acceso_al_Parque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al parque Vuelta al Mundo");
+            Console.WriteLine("Por favor, introduce tu nombre:");
+            string username = "";
+            string cadena = "";
+            username =  Console.ReadLine();
+            Console.WriteLine("Por favor ingresa tu edad:");
+            int edad = 0;
+            cadena = Console.ReadLine();
+            edad = Convert.ToInt32(cadena);
+            if ( edad < 15)
+            {
+                Console.WriteLine("No puedes entrar sin un representante");
+            } 
+            else
+                {
+                    Console.WriteLine("Por favor ingresa tu altura, en metros: ");
+                    double altura = 0.0;
+                    cadena = Console.ReadLine();
+                    altura = Convert.ToDouble(cadena);
+                    
+                    if (altura <= 1.60 )
+                    {
+                        Console.WriteLine("Bienvenido al parque, cuídate y diviértete.");
+
+                     }
+                else {
+                    Console.WriteLine("Diviértete!! Puedes ir a la Montaña Rusa, {0}.", username);}
+                }
+        }   
+    }
+}
